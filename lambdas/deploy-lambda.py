29f9a07addddd4939d28e318d6859472a68e3333
Justin <info@justinlmartin.com>
2020-07-25 20:50:00 -0700

    working pretty well

--- lambdas/deploy-lambda.py
+++ lambdas/deploy-lambda.py
@@ -4,34 +4,79 @@ import mimetypes
 import os
 import io
 import logging
+from string import Template
+import json
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
+def publish_sns(topic, project, status, message, event_id):
+    sns_message = {
+        'project': project,
+        'status': status,
+        'message': message,
+        'event_id': event_id
+    }
+
+    response = topic.publish(
+        # TargetArn=os.environ['SlackRouterLambdaArn'],
+        Message=json.dumps(sns_message),
+        Subject='Deploy Status Update',
+    )
+
+    logger.info(Template('deploy lambda sns response: $response')
+                .safe_substitute(response=response))
+    return
+
+
 def handler(event, context):
+    sns = boto3.resource('sns')
+    status_topic = sns.Topic(os.environ['StatusTopicArn'])
     s3 = boto3.resource('s3')
     artifacts_bucket = s3.Bucket(os.environ['ArtifactBucket'])
     deployment_bucket = s3.Bucket(os.environ['DeployBucket'])
     event_id = event['id']
     project = event['detail']['project-name']
-    logger.info('received event from CodeBuild, project: %s, id: %s' % (project, event_id))
+    logger.info(Template('received event from CodeBuild, project: $project, id: $event_id')
+                .safe_substitute(project=project, event_id=event_id))
     app_name = project.replace('-codebuild', '')
     artifact_path = project.replace('-codebuild', '-artifacts')
     artifact_name = project.replace('-codebuild', '.zip')
     artifact_key = artifact_path + '/' + artifact_name
     artifact_zip = io.BytesIO()
-    logger.info('downloading %s from %s' % (artifact_key, artifacts_bucket))
-    artifacts_bucket.download_fileobj(artifact_key, artifact_zip)
 
-    logger.info('uploading %s to %s' % (app_name, deployment_bucket))
-    with zipfile.ZipFile(artifact_zip) as this_zip:
-        for object_name in this_zip.namelist():
-            upload_key = app_name + '/' + object_name
-            this_object = this_zip.open(object_name)
-            deployment_bucket.upload_fileobj(this_object, upload_key,
-                ExtraArgs={'ContentType': mimetypes.guess_type(object_name)[0]})
+    logger.info(Template('downloading $artifact from $bucket')
+                .safe_substitute(artifact=artifact_key, bucket=artifacts_bucket))
+    try:
+        artifacts_bucket.download_fileobj(artifact_key, artifact_zip)
+    except:
+        message = Template('There was an error downloading $artifact. Event ID: $event_id') \
+            .safe_substitute(artifact=artifact_key, event_id=event_id)
+        logger.error(message)
+        publish_sns(status_topic, app_name, 'FAILED', message, event_id)
+        return
 
-    logger.info('uploading %s complete' % app_name)
+    logger.info(Template('uploading $app to $bucket')
+                .safe_substitute(app=app_name, bucket=deployment_bucket))
+    try:
+        with zipfile.ZipFile(artifact_zip) as this_zip:
+            for object_name in this_zip.namelist():
+                upload_key = app_name + '/' + object_name.replace('src/', '', 1)
+                this_object = this_zip.open(object_name)
+                deployment_bucket.upload_fileobj(this_object, upload_key,
+                                                 ExtraArgs={'ContentType': mimetypes.guess_type(object_name)[0]})
+    except:
+        message = Template('There was an error uploading the objects. Event ID: $event_id') \
+            .safe_substitute(event_id=event_id)
+        logger.error(message)
+        publish_sns(status_topic, app_name, 'FAILED', message, event_id)
+        return
+
+    message = Template('uploading $app complete. Event ID: $event_id') \
+        .safe_substitute(app=app_name, event_id=event_id)
+    logger.info(message)
+    publish_sns(status_topic, app_name, 'SUCCEEDED', message, event_id)
+    return
 
 
--- lambdas/slack-router-lambda.py
+++ lambdas/slack-router-lambda.py
@@ -2,51 +2,88 @@ import boto3
 import json
 import logging
 import os
+from string import Template
 
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
 
-webhookBase = "https://hooks.slack.com/services"
-webhookPath = os.environ['SlackWebHookPath']
-slackMessage = {
-    	"text": "Root Activity Detected",
-    	"attachments": [
-    		{
-    			"text": "message",
-    			"color": "#000000"
-    		}
-    	]
-    }
-
-apiColor = "#345643"
-loginColor = "#abcdef"
-unknownColor = "#cc0606"
+webhook_base = "https://hooks.slack.com/services"
+webhook_path = os.environ['SlackWebHookPath']
+slack_message = {
+    "text": "CodeBuild Status Change",
+    "attachments": [
+        {
+            "text": "",
+            "color": ""
+        }
+    ]
+}
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+emoji_dict = {
+    'IN_PROGRESS': '🤔',
+    'SUCCEEDED': '👍',
+    'FAILED': '😢'
+}
+color_dict = {
+    'IN_PROGRESS': '#e0de43',
+    'SUCCEEDED': '#30b342',
+    'FAILED': '#cc2d1f'
+}
 
-def handler(event, context):
 
-    if event['detail-type'] == "AWS API Call via CloudTrail":
-        slackDetails = "{}\n{}\n{}\n{}\n{}\n{}".format(event['detail']['awsRegion'],event['detail']['eventTime'],event['detail']['eventSource'],event['detail']['eventType'],event['detail']['eventName'],event['detail']['eventID'])
-        slackMessage['attachments'][0]['text'] = slackDetails
-        slackMessage['attachments'][0]['color'] = apiColor
-    elif event['detail-type'] == "AWS Console Sign In via CloudTrail":
-        slackDetails = "{}\n{}\n{}\n{}\n{}\n{}".format(event['detail']['awsRegion'],event['detail']['eventTime'],event['detail']['eventSource'],event['detail']['eventType'],event['detail']['eventName'],event['detail']['eventID'])
-        slackMessage['attachments'][0]['text'] = slackDetails
-        slackMessage['attachments'][0]['color'] = loginColor
-    else:
-        logger.info(event)
-        slackMessage['attachments'][0]['text'] = "RootActivityMonitorLambda was triggered but cause could not be determined"
-        slackMessage['attachments'][0]['color'] = unknownColor
+def send_slack_message(this_id, header, details, color):
+    slack_message['text'] = header
+    slack_message['attachments'][0]['text'] = details
+    slack_message['attachments'][0]['color'] = color
 
-    req = Request("{}{}".format(webhookBase, webhookPath), json.dumps(slackMessage).encode('utf-8'))
+    url = Template('$base$path').safe_substitute(base=webhook_base, path=webhook_path)
+    json_message = json.dumps(slack_message).encode('utf-8')
+    req = Request(url, json_message)
+
     try:
         response = urlopen(req)
         response.read()
-        logger.info("Message posted to slack channel")
+        message = Template('Message posted to slack channel. Event ID: $event_id') \
+            .safe_substitute(event_id=this_id)
+        logger.info(message)
     except HTTPError as e:
-        logger.error("Request failed: %d %s", e.code, e.reason)
+        message = Template('Slack request failed: $code $reason') \
+            .safe_substitute(code=e.code, reason=e.reason)
+        logger.error(message)
     except URLError as e:
-        logger.error("Server connection failed: %s", e.reason)
+        message = Template('Server connection failed: $reason') \
+            .safe_substitute(reason=e.reason)
+        logger.error(message)
+
+    return
+
+
+def handler(event, context):
+    received_template = Template('Received an event of type $type event id $event_id')
+    logger.info(event)
+    event_type = 'cloudwatch-event' if 'id' in event else 'sns-message'
+    if event_type == 'cloudwatch-event':
+        event_id = event['id']
+        project = event['detail']['project-name']
+        status = event['detail']['build-status']
+        details = Template('Project: $project\nStatus: $status $emoji\nEvent ID: $event_id') \
+            .safe_substitute(project=project, status=status, emoji=emoji_dict[status], event_id=event_id)
+        header = 'CodeBuild Status Change'
+        logger.info(received_template.safe_substitute(type=event_type, event_id=event_id))
+        send_slack_message(event_id, header, details, color_dict[status])
+    else:
+        message_id = event['Records'][0]['Sns']['MessageId']
+        sns_message = json.loads(event['Records'][0]['Sns']['Message'])
+        project = sns_message['project']
+        status = sns_message['status']
+        message = sns_message['message']
+        details = Template('Project: $project\nStatus: $status $emoji\nMessage: $message\nSNS Message ID: $message_id') \
+            .safe_substitute(project=project, status=status, emoji=emoji_dict[status], message=message, message_id=message_id)
+        header = 'Deployment Lambda Update'
+        logger.info(received_template.safe_substitute(type=event_type, event_id=message_id))
+        send_slack_message(message_id, header, details, color_dict[status])
+
+    return
